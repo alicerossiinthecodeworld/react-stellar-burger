5b11ba1f084f9ae838310995c616d01e29307871
Anastasia Proskuryakova <anproskuryakova@ozon.ru>
2023-09-16 18:45:30 +0600

    deleted constants

--- src/utils/constants.js
+++
@@ -1,3 +0,0 @@
-export const anchorSauce = document.querySelectorAll('#sauceAnc')
-export const anchorBuns = document.querySelectorAll('#bunsAnc')
-export const anchorFillings = document.querySelectorAll('#fillingsAnc')
