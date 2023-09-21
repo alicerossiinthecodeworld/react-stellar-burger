31673305e012ffac1f8763e1f1a1d05578e166d4
Anastasia Proskuryakova <anproskuryakova@ozon.ru>
2023-09-21 21:18:19 +0600

    fixed index.js

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -2,8 +2,6 @@ import styles from './app.module.css';
 import AppHeader from '../app-header/app-header';
 import BurgerIngredients from '../burger-ingredients/burger-ingredients';
 import BurgerConstructor from '../burger-constructor/burger-constructor';
-import { Provider } from 'react-redux'; 
-import store from '../../services/store';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 
