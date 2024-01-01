914d5c8c4a4693f572eabdb1669a18da777ec73b
Anastasia Proskuryakova <anproskuryakova@ozon.ru>
2024-01-01 17:32:05 +0100

    made feed

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -11,6 +11,7 @@ import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { getSavedUserData, loginSuccess } from '../../services/auth-slice';
 import { fetchIngredients } from '../../services/ingredient-slice';
+import FeedPage from '../../pages/feed-page/feed-page';
 
 export default function App(){
   const dispatch = useDispatch();
@@ -54,6 +55,10 @@ export default function App(){
             </ProtectedRouteElement>
           }
         />
+        <Route
+          path="/feed"
+          element={<FeedPage />} 
+        />  
       </Routes>
     </Router>
   );
